92a97df51f3f39cbb0c99b19e84f1f731d7526fa
Juan-Pablo Velez <jpvelez@spotify.com>
2018-07-18 17:11:24 -0400

    Use locationtech JTS and jts2json, rewrite geoJSONFile sink with them

--- build.sbt
+++ build.sbt
@@ -9,6 +9,11 @@ libraryDependencies ++= Seq(
   "com.spotify" %% "scio-test" % "0.5.2" % "test",
   "org.apache.beam" % "beam-runners-direct-java" % "2.4.0",
   "org.apache.beam" % "beam-runners-google-cloud-dataflow-java" % "2.4.0",
-  "org.datasyslab" % "geospark" % "1.1.3",
+//  "org.datasyslab" % "geospark" % "1.1.3",
+  "org.locationtech.jts" % "jts-core" % "1.15.0",
+  "org.locationtech.jts" % "jts-io" % "1.15.0",
+  "org.wololo" % "jts2geojson" % "0.12.0",
+//  "com.conveyal" % "geobuf-java" % "1.0-SNAPSHOT",
 )
 
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
--- src/main/scala/com/spotify/scio/spatial/SpatialConverters.scala
+++ src/main/scala/com/spotify/scio/spatial/SpatialConverters.scala
@@ -1,19 +1,23 @@
 package com.spotify.scio.spatial
 
 import com.spotify.scio.ScioContext
-import com.vividsolutions.jts.geom.Geometry
-import org.datasyslab.geospark.enums.FileDataSplitter
-import org.datasyslab.geospark.formatMapper.FormatMapper
-
-import scala.concurrent.duration.Duration
+import com.spotify.scio.values.SCollection
+import org.locationtech.jts.geom.Geometry
+import org.wololo.jts2geojson.GeoJSONReader
 
 object SpatialConverters {
 
   implicit class SpatialScioContext(sc: ScioContext) {
 
-    def geoJSONFile[T <: Geometry](path: String) = {
-      val fm = new FormatMapper(FileDataSplitter.GEOJSON, false)
-      sc.textFile(path).map(fm.readGeoJSON)
+    // KryoAtomicCoder serializes JTS Geometry instances to different values. Must be one of these:
+    // http://javadox.com/com.vividsolutions/jts/1.13/serialized-form.html
+    // TODO replicate the issue kicked up by org.apache.beam.runners.direct.ImmutabilityCheckingBundleFactory
+    // Then, identify the problem field. Then, write a KSerializer that deals with this field,
+    // and register it with an IKryoRegistar once you test the KSerializer. byte arrays should be
+    // the same...
+    def geoJSONFile(path: String): SCollection[Geometry] = {
+      lazy val reader = new GeoJSONReader
+      sc.textFile(path).map(reader.read)
     }
 
   }
