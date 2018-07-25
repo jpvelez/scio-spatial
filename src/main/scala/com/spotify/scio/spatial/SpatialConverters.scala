2fc6d16b336627fb3f93b8084449d75dc4d16a48
Juan-Pablo Velez <jpvelez@spotify.com>
2018-07-25 18:10:18 -0400

    Parse any geoJSON object, delimited or no, into corresponding JTS Geometry

--- src/main/scala/com/spotify/scio/spatial/SpatialConverters.scala
+++ src/main/scala/com/spotify/scio/spatial/SpatialConverters.scala
@@ -1,20 +1,18 @@
 package com.spotify.scio.spatial
 
 import com.spotify.scio.ScioContext
-import com.spotify.scio.transforms.DoFnWithResource
 import com.spotify.scio.values.SCollection
-import org.locationtech.jts.geom.{Geometry, Point}
+import org.locationtech.jts.geom.Geometry
+import org.wololo.geojson._
+import org.wololo.geojson
 import org.wololo.jts2geojson.GeoJSONReader
 
 import scala.reflect.ClassTag
-import scala.util.Try
 
 object SpatialConverters {
 
   implicit class SpatialScioContext(sc: ScioContext) {
 
-    // TODO make proper pipeline test
-    // TODO add serializers for point, check that sink works there too. write test.
     // TODO check that it fails if you have polygons but expect points, and vice versa.
     // TODO if it does fail, implement the either and update the test. write test.
     // TODO expect behavior: should only container line separated features of the same type. if
@@ -22,13 +20,19 @@ object SpatialConverters {
     // you get a runtime exception, or an a right-valued either.
     // TODO check if jts2geojson or jts.io can cast directly to the right geomtype?
     // TODO add serializers linear ring, line string, and multi variants.
-    // TODO deal with geojson that has feature collections.
 
     def geoJSONFile[T <: Geometry: ClassTag](path: String): SCollection[T] = {
-//    SCollection[Either[Throwable, T]] = {
       lazy val reader = new GeoJSONReader
-      sc.textFile(path).map { l => reader.read(l).asInstanceOf[T] }
-//      sc.textFile(path).map { l => Try(reader.read(l).asInstanceOf[T]).toEither }
+      sc.textFile(path)
+        .flatMap[geojson.Geometry] {
+        GeoJSONFactory.create(_) match {
+          case fc: FeatureCollection => fc.getFeatures.map(_.getGeometry)
+          case f: Feature => Some(f.getGeometry)
+          case gc: GeometryCollection => gc.getGeometries
+          case geom: geojson.Geometry => Some(geom)
+        }
+      }
+        .map( geom => reader.read(geom).asInstanceOf[T] )
     }
   }
 }
