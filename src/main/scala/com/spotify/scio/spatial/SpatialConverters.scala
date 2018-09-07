371ab5fbeb133ee3fce969c61a92292763db31bd
Juan-Pablo Velez <jpvelez@spotify.com>
2018-09-07 19:15:03 -0400

    Add analysis of geospark internals

--- src/main/scala/com/spotify/scio/spatial/SpatialConverters.scala
+++ src/main/scala/com/spotify/scio/spatial/SpatialConverters.scala
@@ -25,6 +25,34 @@ object SpatialConverters {
     // Test one real-world geojson data.
 
 
+    // TODO ANALYZE GEOSPARK
+    // Partitioning: you partition with a partitioner. Partitioners break up the space into spatial partitions,
+    // and then decide geometry lies inside of one partition (in the case of points) or maybe several (in the case of po
+    // polygons). Then you use Spark's partitionBy to basically shuffle each geometry to a node that houses all the
+    // geometries that fall ~within~ that spatial partition. For as yet unknown reasons, every partition requires a some
+    // bounding box of all the data (the latter makes sense) and/or some sample of the data (presumably to set the partitions).
+    // Both require doing a full pass of the data: to get the spatial extent, and to get the approximate total count
+    // of elements, from which you can figure out how many samples you need, and then sample.
+
+    // Index building: using mapPartitions, you iterate through the geometries in every partition (spatially paritioned
+    // or random), and insert them into one of many indexes in JTS. You insert their envelope, and the geom itself.
+    // Then, you get an RDD of Indexes. The indexes now house all the points. This RDD is typically persisted.
+    // Now, you can query the index in different ways: KNN to a point, points within a bounding box, distance queries,
+    // etc.
+
+    // Scio Implementation.
+    // 1. Sketch (first pass): Do first pass over the data, using Algebird aggregators - get max x, min x, max y, min y, and approximate
+    // count in one pass.
+    // 2. Sample (second pass): If you need to sample, do a second pass over the data to sample. Scio has sampling methods.
+    // 3. Build partitioner: build the partitioner object using sketches and maybe sample.
+    // 4. Partition (third pass): shuffle each geometry to the appropriate partition using groupby and the partitioner.
+    // 5. Build index: insert each element into the index, and save the indexes to GCS? Or somehow pass them with sideinput?
+    // 6. Query: now that you have the index, look up the bounding box (range query), point (knn), or SOMEHOW
+    // go through the elements of the left hand table and do lookups in the indexes. Do they have to end up on the same
+    // node too?
+    // TODO HOW DOES GEOSPARK HANDLE THIS CASE? KEEP RESEARCHING THIS, AND ALSO CLARIFY THE SCIO HANDOFFS, UNTIL YOURE SURE ITS FEASIBLE.
+
+
     def geoJSONFile[T <: Geometry: ClassTag](path: String): SCollection[T] = {
       lazy val reader = new GeoJSONReader
       sc.textFile(path)
